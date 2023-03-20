3c48741e1818c5d7cda842b492962c23f29a697a
MarsMwau <marthamumbua16@gmail.com>
2023-03-20 11:41:00 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+let employee = {
+    name : 'Kilua',
+    streetAdress : 57885
+}
+
+function updateEmployeeWithKeyAndValue(object, key, value) {
+    return {
+        ...employee,
+        [key]: value, 
+    };
+}
+    let updatedEmployee = updateEmployeeWithKeyAndValue(employee, 'streetAddress', 89976);
+    
+function  destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+destructivelyUpdateEmployeeWithKeyAndValue(employee, 'streetAddress', 45689);
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  updatedEmployee = deleteFromEmployeeByKey(employee, 'name');
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
